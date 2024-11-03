88809316c5b9fb47bc6c747900b605b6ad96b7e7
PABLO ROS PROFE <pros7@xtec.cat>
2024-11-03 12:35:37 +0100

    correcció estils taula

--- src/components/studentList.jsx
+++ src/components/studentList.jsx
@@ -29,26 +29,30 @@ const StudentList = ({
     <table className="m-3 table-auto rounded-lg">
       <thead className="bg-blue-500 p-2 px-4 py-2 font-bold text-white">
         <tr>
-          <th className="rounded-l-lg px-4 py-2">Nom</th>
+          <th className="rounded-tl-lg px-4 py-2">Nom</th>
           <th className="px-4 py-2">Cognoms</th>
           <th className="px-4 py-2">Estudis</th>
           <th className="px-4 py-2">Correu</th>
-          <th className="rounded-r-lg px-4 py-2">Accions</th>
+          <th className="rounded-tr-lg px-4 py-2">Accions</th>
         </tr>
       </thead>
       <tbody>
         {items.map((item) => (
-          <tr key={item.key}>
+          <tr key={item.key} className="h-12">
+            {' '}
+            {/* Set a fixed height for each row */}
             <td className="border px-4 py-2">{item.fname}</td>
             <td className="border px-4 py-2">{item.lname}</td>
             <td className="border px-4 py-2">{item.programa}</td>
             <td className="border px-4 py-2">{item.email}</td>
-            <td className="flex border px-4 py-2">
-              <MdEdit className="cursor-pointer text-blue-500 hover:text-blue-700" />
-              <MdDelete
-                className="cursor-pointer hover:text-red-500"
-                onClick={() => handleDelete(item.key, item.programa)}
-              />{' '}
+            <td className="border px-4 py-2 text-center">
+              <div className="flex items-center justify-center">
+                <MdEdit className="mx-2 cursor-pointer text-blue-500 hover:text-blue-700" />
+                <MdDelete
+                  className="mx-2 cursor-pointer hover:text-red-500"
+                  onClick={() => handleDelete(item.key, item.programa)}
+                />
+              </div>
             </td>
           </tr>
         ))}
